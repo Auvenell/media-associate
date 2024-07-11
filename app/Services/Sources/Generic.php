7a779dc956ddf9b2de2ea8b1d5d45f1480b2e7b0
Auvenell <auvenell@gmail.com>
2024-07-11 04:18:38 -0400

    updated site capture & html conversion for generic site handler

--- app/Services/Sources/Generic.php
+++ app/Services/Sources/Generic.php
@@ -12,7 +12,18 @@ class Generic
             exit('Login Failed');
         }
 
-        $command = 'osascript Development/media-associate/site-capture.scpt' . ' ' . $url;
+        $command = 'osascript Development/media-associate/site-capture-html.scpt ' . '"' . $url . '"';
+        echo $ssh->exec($command);
+    }
+
+    public function convertHtmltoTxt()
+    {
+        $ssh = new NetSSH2(env('MAC_SERVER_IP', '192.168.1.236'));
+        if (!$ssh->login(env('MAC_SERVER_USER'), env('MAC_SERVER_PASSWD'))) {
+            exit('Login Failed');
+        }
+
+        $command = '/opt/homebrew/bin/node Development/html-to-text/html-to-text.js';
         echo $ssh->exec($command);
     }
 }
